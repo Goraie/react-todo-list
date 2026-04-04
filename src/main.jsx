165fdc5d8b6382f7d56534d36dd48d2e85cfef90
Goraie <eoarchibasov@stud.etu.ru>
2026-04-04 17:31:46 +0300

    change architecture to FSD

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
+import App from '@//app'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './styles'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
